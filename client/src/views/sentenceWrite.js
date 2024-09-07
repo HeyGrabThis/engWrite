9be2b91a3afa33fe5dba678b21569faf9ed18089
HeyGrabThis <qkqajrrhtj@naver.com>
2024-09-07 16:37:08 +0900

    삭제버튼디자인수정

--- client/src/views/sentenceWrite.js
+++ client/src/views/sentenceWrite.js
@@ -107,8 +107,8 @@ const SentenceWrite = () => {
           <div className='delBtn' style={{ visibility: 'hidden' }}>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              width='32'
-              height='32'
+              width='24'
+              height='24'
               viewBox='0 0 24 24'
               fill='none'
             >
@@ -126,8 +126,8 @@ const SentenceWrite = () => {
           <div className='delBtn'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              width='32'
-              height='32'
+              width='24'
+              height='24'
               viewBox='0 0 24 24'
               fill='none'
             >
