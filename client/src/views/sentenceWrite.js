23b1ae39a00ee6fe0f0cdd180768b413d465fd95
HeyGrabThis <qkqajrrhtj@naver.com>
2024-09-16 17:53:12 +0900

    성공컴포넌트 기본

--- client/src/views/sentenceWrite.js
+++ client/src/views/sentenceWrite.js
@@ -6,7 +6,6 @@ const SentenceWrite = () => {
   let [selected, setSelected] = useState([]); //선택된 상태 저장
   let [selCopy, setSelCopy] = useState([]); // 몇개의 상태가 필요한지 false배열로 카피
   const changeWordDesign = (idx) => {
-    console.log(selected);
     if (selected[idx] === false) {
       let copy = [...selected];
       copy[idx] = true;
@@ -64,6 +63,13 @@ const SentenceWrite = () => {
       }
     }
   };
+  //성공 컴포넌트
+  const SuccessSentence = () => {
+    let [engFullSentence, setEngFullSentence] = useState(
+      engSentenceAns.join(' ')
+    );
+    return <div className={`successSen`}>{engFullSentence}</div>;
+  };
   //흔드는 애니메이션 상태
   let [vibeState, setVibeState] = useState(false);
   //단어 배열 상태에 따라 정답처리 및 오답처리
@@ -71,7 +77,7 @@ const SentenceWrite = () => {
     if (tureWords === 0) {
       return;
     } else if (tureWords === 1) {
-      //성공 페이지 띄우고 다음으로 넘기기
+      //성공 컴포넌트 띄우기
     } else if (tureWords === 2) {
       //다시 하게끔
       setVibeState(true); //바이브레이션 주고
@@ -169,8 +175,8 @@ const SentenceWrite = () => {
               fill='none'
             >
               <path
-                fill-rule='evenodd'
-                clip-rule='evenodd'
+                fillRule='evenodd'
+                clipRule='evenodd'
                 d='M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM8.29289 8.29289C8.68342 7.90237 9.31658 7.90237 9.70711 8.29289L12 10.5858L14.2929 8.29289C14.6834 7.90237 15.3166 7.90237 15.7071 8.29289C16.0976 8.68342 16.0976 9.31658 15.7071 9.70711L13.4142 12L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L12 13.4142L9.70711 15.7071C9.31658 16.0976 8.68342 16.0976 8.29289 15.7071C7.90237 15.3166 7.90237 14.6834 8.29289 14.2929L10.5858 12L8.29289 9.70711C7.90237 9.31658 7.90237 8.68342 8.29289 8.29289Z'
                 fill='rgb(52, 52, 70)'
               />
@@ -190,6 +196,7 @@ const SentenceWrite = () => {
                 );
               })
             : null}
+          <SuccessSentence></SuccessSentence>
           <div className='delBtn' onClick={deleteWords}>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -199,8 +206,8 @@ const SentenceWrite = () => {
               fill='none'
             >
               <path
-                fill-rule='evenodd'
-                clip-rule='evenodd'
+                fillRule='evenodd'
+                clipRule='evenodd'
                 d='M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM8.29289 8.29289C8.68342 7.90237 9.31658 7.90237 9.70711 8.29289L12 10.5858L14.2929 8.29289C14.6834 7.90237 15.3166 7.90237 15.7071 8.29289C16.0976 8.68342 16.0976 9.31658 15.7071 9.70711L13.4142 12L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L12 13.4142L9.70711 15.7071C9.31658 16.0976 8.68342 16.0976 8.29289 15.7071C7.90237 15.3166 7.90237 14.6834 8.29289 14.2929L10.5858 12L8.29289 9.70711C7.90237 9.31658 7.90237 8.68342 8.29289 8.29289Z'
                 fill='rgb(52, 52, 70)'
               />
